5c824f76dc3bce4b0fd4c86bab325b08ea97df39
Ignite Corp. <adriankxavier@gmail.com>
2024-10-18 18:03:17 -0300

    up

--- public/dashboard/js/login_validate.js
+++ public/dashboard/js/login_validate.js
@@ -1,61 +1,76 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevenir o comportamento padrão do formulário
 
     // Pegar valores dos campos
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
     // Validação simples do formulário
-    if (username === '' || password === '') {
-        document.getElementById('loginMessage').textContent = 'Por favor, preencha todos os campos.';
-        console.log('Campos de usuário ou senha estão vazios.');
-        return;
+    if (username === "" || password === "") {
+      document.getElementById("loginMessage").textContent =
+        "Por favor, preencha todos os campos.";
+      console.log("Campos de usuário ou senha estão vazios.");
+      return;
     }
 
-    console.log('Iniciando requisição de login...');
+    console.log("Iniciando requisição de login...");
 
     // Fazer requisição de login via axios
-    axios.post('http://localhost:8000/backend/login.php', {
-        username: username,
-        password: password
-    })
-    .then(function(response) {
-        // Login bem-sucedido, salvar dados no localForage
-        const data = response.data;
-        console.log('Login bem-sucedido, recebendo dados:', data);
+    // axios.post('http://localhost:8000/public/backend/login.php', {
+    //     username: username,
+    //     password: password
+    // })
+    // .then(function(response) {
+    //     Login bem-sucedido, salvar dados no localForage
+    //     const data = response.data;
+    //     console.log('Login bem-sucedido, recebendo dados:', data);
 
-        localforage.setItem('jwt', data.jwt).then(function() {
-            console.log('JWT armazenado com sucesso!');
-            // Redirecionar para o dashboard
-            document.getElementById('loginContainer').style.display = 'none';
-            document.getElementById('dashboardContainer').style.display = 'block';
-            console.log('Dashboard exibido.');
-        });
-        localforage.setItem('username', data.username).then(function() {
-            console.log('Nome de usuário armazenado com sucesso!');
-        });
+    //     localforage.setItem('jwt', data.jwt).then(function() {
+    //         console.log('JWT armazenado com sucesso!');
+    //         Redirecionar para o dashboard
+    //         document.getElementById('loginContainer').style.display = 'none';
+    //         document.getElementById('dashboardContainer').style.display = 'block';
+    //         console.log('Dashboard exibido.');
+    //     });
+    //     localforage.setItem('username', data.username).then(function() {
+    //         console.log('Nome de usuário armazenado com sucesso!');
+    //     });
 
-        // Exibir mensagem de sucesso
-        document.getElementById('loginMessage').textContent = 'Login realizado com sucesso!';
-    })
-    .catch(function(error) {
-        // Tratar erro de login
-        document.getElementById('loginMessage').textContent = 'Usuário ou senha incorretos!';
-        console.log('Erro ao fazer login:', error);
-    });
-});
+    //     Exibir mensagem de sucesso
+    //     document.getElementById('loginMessage').textContent = 'Login realizado com sucesso!';
+    // })
+    // .catch(function(error) {
+    //     Tratar erro de login
+    //     document.getElementById('loginMessage').textContent = 'Usuário ou senha incorretos!';
+    //     console.log('Erro ao fazer login:', error);
+    // });
+    const request = await fetch(
+      "http://localhost:8000/public/backend/login.php",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }
+    );
+    const requestDatas = await request.json();
+    console.log("aqui", requestDatas);
+  });
 
 // Verificar se o usuário está logado ao carregar a página
-localforage.getItem('jwt').then(function(jwt) {
-    if (jwt) {
-        // Se o JWT estiver presente, mostrar o dashboard
-        document.getElementById('loginContainer').style.display = 'none';
-        document.getElementById('dashboardContainer').style.display = 'block';
-        console.log('Usuário está logado, exibindo dashboard.');
-    } else {
-        // Caso contrário, mostrar o formulário de login
-        document.getElementById('loginContainer').style.display = 'block';
-        document.getElementById('dashboardContainer').style.display = 'none';
-        console.log('Usuário não está logado, exibindo formulário de login.');
-    }
+localforage.getItem("jwt").then(function (jwt) {
+  if (jwt) {
+    // Se o JWT estiver presente, mostrar o dashboard
+    document.getElementById("loginContainer").style.display = "none";
+    document.getElementById("dashboardContainer").style.display = "block";
+    console.log("Usuário está logado, exibindo dashboard.");
+  } else {
+    // Caso contrário, mostrar o formulário de login
+    document.getElementById("loginContainer").style.display = "block";
+    document.getElementById("dashboardContainer").style.display = "none";
+    console.log("Usuário não está logado, exibindo formulário de login.");
+  }
 });
